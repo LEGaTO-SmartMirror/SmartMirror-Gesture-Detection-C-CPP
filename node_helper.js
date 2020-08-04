131f86968104cf92db402e1573291f35bd88f217
Nils Kucza <nkucza@cit-ec.uni-bielefeld.de>
2020-08-04 12:52:10 +0200

    update path to exe

--- node_helper.js
+++ node_helper.js
@@ -8,27 +8,37 @@ module.exports = NodeHelper.create({
 
 	cApp_start: function () {
 		const self = this;
-		self.gestureDet = spawn('modules/' + this.name + '/gesture-detection/build/gesture_detection',['modules/' + this.name + '/gesture-detection/build', self.config.image_width, self.config.image_height]);
+		self.gestureDet = spawn('modules/' + this.name + '/gesture_detection/build/gesture_detection',['modules/' + this.name + '/gesture_detection/build', self.config.image_width, self.config.image_height]);
 		self.gestureDet.stdout.on('data', (data) => {
-			try{
-				var parsed_message = JSON.parse(`${data}`)
+			
+			var data_chunks = `${data}`.split('\n');
+			data_chunks.forEach( chunk => {
 
-				if (parsed_message.hasOwnProperty('DETECTED_GESTURES')){
-					//console.log("[" + self.name + "] detected object: " + parsed_message.detected.name + " center in "  + parsed_message.detected.center);
-					self.sendSocketNotification('DETECTED_GESTURES', parsed_message);
-				}else if (parsed_message.hasOwnProperty('GESTURE_DET_FPS')){
-					//console.log("[" + self.name + "] gesture detection fps: " + JSON.stringify(parsed_message));
-					self.sendSocketNotification('GESTURE_DET_FPS', parsed_message.GESTURE_DET_FPS);
-				}else if (parsed_message.hasOwnProperty('STATUS')){
-					console.log("[" + self.name + "] status received: " + JSON.stringify(parsed_message));
+				if (chunk.length > 0) {
+	
+				try{
+					var parsed_message = JSON.parse(chunk)
+
+					if (parsed_message.hasOwnProperty('DETECTED_GESTURES')){
+						//console.log("[" + self.name + "] Gestures detected : " + parsed_message);
+						self.sendSocketNotification('DETECTED_GESTURES', parsed_message);
+					}else if (parsed_message.hasOwnProperty('GESTURE_DET_FPS')){
+						console.log("[" + self.name + "] " + JSON.stringify(parsed_message));
+						self.sendSocketNotification('GESTURE_DET_FPS', parsed_message.GESTURE_DET_FPS);
+					}else if (parsed_message.hasOwnProperty('STATUS')){
+						console.log("[" + self.name + "] status received: " + JSON.stringify(parsed_message));
+					}
 				}
-			}
-			catch(err) {
-				
-				//console.log(err)
-			}
-  			//console.log(`stdout: ${data}`);
+				catch(err) {	
+					//console.log(err)
+				}
+  				//console.log(chunk);
+				}
+			});
 		});	
+
+
+
 		exec(`renice -n 20 -p ${self.gestureDet.pid}`,(error,stdout,stderr) => {
 				if (error) {
 					console.error(`exec error: ${error}`);
